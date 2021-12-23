db28bd303d8161ef43d13ba3d62c5deab2cfae7f
Samy Chabbi <92692913+Sch57@users.noreply.github.com>
2021-12-23 15:45:34 +0100

    Add files via upload

--- app.js
+++ app.js
@@ -1,39 +1,88 @@
-let ville = 'Strasbourg';
+// Api meteo geolocaliser
 
-recevoirTemperature(ville);
+let villeChoisie;
+
+if ('geolocation' in navigator) {
+	navigator.geolocation.watchPosition(
+		(position) => {
+			const url =
+				'https://api.openweathermap.org/data/2.5/weather?lat=' +
+				position.coords.latitude +
+				'&lon=' +
+				position.coords.longitude +
+				'&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
+
+			let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
+			requete.open('GET', url); // Nous récupérons juste des données
+			requete.responseType = 'json'; // Nous attendons du JSON
+			requete.send(); // Nous envoyons notre requête
+
+			// Dès qu'on reçoit une réponse, cette fonction est executée
+			requete.onload = function () {
+				if (requete.readyState === XMLHttpRequest.DONE) {
+					if (requete.status === 200) {
+						let reponse = requete.response;
+						// console.log(reponse);
+						let temperature = reponse.main.temp;
+						let ville = reponse.name;
+						// console.log(temperature);
+						document.querySelector('#temperature_label').textContent =
+							temperature;
+						document.querySelector('#ville').textContent = ville;
+					} else {
+						alert('Un problème est intervenu, merci de revenir plus tard.');
+					}
+				}
+			};
+		},
+		erreur,
+		options
+	);
+
+	var options = {
+		enableHighAccuracy: true,
+	};
+} else {
+	villeChoisie = 'Paris';
+	recevoirTemperature(villeChoisie);
+}
 
 let changerDeVille = document.querySelector('#changer');
-
 changerDeVille.addEventListener('click', () => {
-	ville = prompt('Quelle ville souhaitez vous voir ?');
-	recevoirTemperature(ville);
+	villeChoisie = prompt('Quelle ville souhaitez-vous voir ?');
+	recevoirTemperature(villeChoisie);
 });
+
+function erreur() {
+	villeChoisie = 'Paris';
+	recevoirTemperature(villeChoisie);
+}
 
 function recevoirTemperature(ville) {
 	const url =
 		'https://api.openweathermap.org/data/2.5/weather?q=' +
 		ville +
-		'&appid=034738d07f89e457433a7b144f264ba4&units=metric';
+		'&appid=dc8c9152e8adaad0ec8bf635818c0d42&units=metric';
 
-	//
+	let requete = new XMLHttpRequest(); // Nous créons un objet qui nous permettra de faire des requêtes
+	requete.open('GET', url); // Nous récupérons juste des données
+	requete.responseType = 'json'; // Nous attendons du JSON
+	requete.send(); // Nous envoyons notre requête
 
-	let requete = new XMLHttpRequest();
-	requete.open('GET', url);
-	requete.responseType = 'json';
-	requete.send();
-
+	// Dès qu'on reçoit une réponse, cette fonction est executée
 	requete.onload = function () {
 		if (requete.readyState === XMLHttpRequest.DONE) {
 			if (requete.status === 200) {
 				let reponse = requete.response;
+				// console.log(reponse);
 				let temperature = reponse.main.temp;
 				let ville = reponse.name;
+				// console.log(temperature);
 				document.querySelector('#temperature_label').textContent = temperature;
 				document.querySelector('#ville').textContent = ville;
 			} else {
-				alert('Un problème est survenu, merci de revenir plus tard');
+				alert('Un problème est intervenu, merci de revenir plus tard.');
 			}
 		}
-		console.log(reponse);
 	};
 }
